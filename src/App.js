b67be3890cd495974f4a10bd821e2a761551d7e4
jhmlee <jhmlee07@gmail.com>
2023-01-28 16:17:41 -0500

    google oauth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
+
+
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [ profile, setProfile ] = useState([]);
+  const clientId = "803583339617-v3v2k7d19bhl5leqkbhbj1fdk6e1lm52.apps.googleusercontent.com"
+
+
+const onFailure = (err) => {
+  console.log('failed', err);
+};
+
+const logOut = () => {
+  setIsLoggedIn(false);
+  setProfile(null);
+};
+const onSuccess = (res) => {
+  setProfile(res.profileObj);
+  setIsLoggedIn(true);
+};
+
   return (
     <div className="App">
       <header className="App-header">
+      <div>
+            {profile ? (
+                <div>
+                    <p>Hi, {profile.name}</p>
+                    <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
+                </div>
+            ) : (
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign in with Google"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                />
+            )}
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -23,3 +62,4 @@ function App() {
 }
 
 export default App;
+
